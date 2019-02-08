f99db9a0e1774874ac0ed4668bb939ba71bfd8ee
dgigafox <darren@mergecommit.com>
2019-02-08 17:28:27 +0800

    Refactor user header

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -9,3 +10,13 @@ export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts())
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+}
